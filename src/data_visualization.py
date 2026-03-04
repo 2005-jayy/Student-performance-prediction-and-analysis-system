31ed6b2d691c257afc87a40714e2d51e00cf6a17
Saurabh <saurabh.p7140@gmail.com>
2026-03-04 09:22:30 -0800

    just a try tommor i will commite all the visualizetion

---
+++ src/data_visualization.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import mathplotlib.pyplot as plt
+
+
+df = pd.read_csv("D:\Student-performance-prediction-and-analysis-system\data\cleaned_student_dataset.csv")
+
+print(df.head())
