3575c4e9797878783b23bbf10b3afa23d851f664
Saurabh <saurabh.p7140@gmail.com>
2026-03-12 11:35:53 -0700

    update

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -24,8 +24,19 @@ plt.ylabel("marks obtained")
 plt.show()
 
 #now we will see the comparison of marks on bases of gender
-plt.boxplot(df["Marks"] by=df["gender"])
+plt.boxplot(df["Marks"]by= df["gender"])
 plt.title("comparison of marks on bases of gender")
 plt.xlabel("gender")
 plt.ylabel("Marks obtained")
+plt.show()
+
+
+#now the  time for the corelation heatmap to identify the corelation between numeric features in our data set
+plt.figure()
+corr = df.corr(numeric_only=True)
+plt.imshow(corr)
+plt.colorbar()
+plt.xticks(range(len(corr)), corr.columns, rotation=90)
+plt.yticks(range(len(corr)),corr.columns)
+plt.title("heatmap")
 plt.show()
